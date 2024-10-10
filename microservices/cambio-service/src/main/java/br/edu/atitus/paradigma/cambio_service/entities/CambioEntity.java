25a9be2d4f1d4354903a6faced7a157168faa2b3
jmdambros <jmcdambros@gmail.com>
2024-10-10 18:31:30 -0300

    cambio-service

---
+++ microservices/cambio-service/src/main/java/br/edu/atitus/paradigma/cambio_service/entities/CambioEntity.java
@@ -0,0 +1,10 @@
+package br.edu.atitus.paradigma.cambio_service.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table (name = "cambio")
+public class CambioEntity {
+
+}
