99d3263556f4ee67387bf91e11309569f4e69ba4
jmdambros <jmcdambros@gmail.com>
2024-11-21 23:25:18 -0300

    Correção ApiGateway + Compose.yml

--- microservices/api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
+++ microservices/api-gateway-service/src/main/java/br/edu/atitus/paradigma/api_gateway_service/configs/ApiGatewayConfig.java
@@ -7,17 +7,33 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApiGatewayConfig {
-	
-	@Bean
-	RouteLocator getGatewayRouter(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(route -> route
-						.path("/cambio-service/**")
-						.uri("lb://cambio-service"))
-				.route(route -> route
-						.path("/produto-service/**")
-						.uri("lb://produto-service"))
-				.build();
-		
+		@Bean
+		RouteLocator getRoutes(RouteLocatorBuilder builder) {
+			return builder.routes()
+					.route(rota -> rota
+							.path("/get")
+							.filters(f -> f
+									.addRequestHeader("Usuario", "João Marcelo Dambros")
+									)
+							.uri("http://httpbin.org"))
+					.route(rota -> rota
+							.path("/cambio-service/**")
+							.filters(f -> f
+									.addRequestHeader("Usuario", "João Marcelo Dambros")
+									)
+							.uri("lb://cambio-service"))
+					.route(rota -> rota
+							.path("/produto-service/**")
+							.filters(f -> f
+									.addRequestHeader("Usuario", "João Marcelo Dambros")
+									)
+							.uri("lb://produto-service"))
+					.route(rota -> rota
+							.path("/saudacao-service/**")
+							.filters(f -> f
+									.addRequestHeader("Usuario", "João Marcelo Dambros")
+									)
+							.uri("lb://saudacao-service"))
+					.build();
+		}
 	}
-}
